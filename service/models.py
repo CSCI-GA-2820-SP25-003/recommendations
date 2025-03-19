df00fb3c2339ee065e6dd1bc5df320324f1ecc99
twisha-a <twishasahay@gmail.com>
2025-03-19 07:42:44 +0000

    removed pylint disabled in models.py

--- service/models.py
+++ service/models.py
@@ -10,7 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 logger = logging.getLogger("flask.app")
 
 # Create the SQLAlchemy object to be initialized later in init_db()
-db = SQLAlchemy()  # pylint: disable=R0903
+db = SQLAlchemy()  
 
 
 class DataValidationError(Exception):
